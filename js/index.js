38a24e3f0e34b9544c853c5b0af574e6dd22ed40
klymash <klymash.vit@gmail.com>
2019-08-21 20:40:25 +0300

    Added files

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('#slider').slick(
+        {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 1500,
+            speed: 2000,
+            arrows: false,
+            infinite: true
+           
+        }
+    );
+});
